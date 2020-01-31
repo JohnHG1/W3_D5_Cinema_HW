1ff1b8c93f156fd11752f15b8c5d6b4ffd51d8a2
JohnHG1 <honisz@yahoo.com>
2020-01-31 12:00:07 +0000

    Initial commit

---
+++ db/console.rb
@@ -0,0 +1,9 @@
+require_relative('models/ticket')
+require_relative('models/film')
+require_relative('models/customer')
+
+require('pry-byebug')
+
+# Ticket.delete_all()
+# Film.delete_all()
+# Customer.delete_all()
